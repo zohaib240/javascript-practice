c20aadb4b0859ef006443a4e37c6936ae620e9ad
zohaib240 <zohaibjs123@gmail.com>
2024-03-13 18:01:11 +0500

    index file and style.css created

---
+++ app.js
@@ -0,0 +1,232 @@
+
+//var userName = 'Zohaib';
+//console.log(userName);
+
+//var num1 =10
+//var num2 ='5'
+//var num3 =20
+//console.log(num1 + num2 + num3) 
+//console.log(typeof(num2))
+
+
+//var username =prompt("Enter first your name");
+//console.log(username);
+//console.log(10 != '10');
+
+
+//var username ='zohaib'
+//document.write( '<h5>' +username+ '</h5>').
+
+
+// var  num1 =10; 
+// var  num2 =20;
+
+
+// console.log( "num1 * num2=", num1 * num2);
+// console.log( "num1 % num2=", num1 % num2);
+// console.log( "num1 / num2=", num1 / num2);
+// console.log( "num1 + num2=", num1 + num2);
+// console.log( "num1 ** num2=", num1 ** num2);
+
+// uniary operator
+
+
+// var  a=10; 
+// var   b=20;
+
+// console.log( "a =" ,a, "& b =" ,b);
+// ++a;
+// ++b;
+// console.log('a=' ,a)
+// console.log('b=' ,b)
+
+// Assimnt operaters
+
+// var  a=10; 
+// var   b=20;
+
+// console.log( "a =" ,a, "& b =" ,b);
+
+// a += 4;
+// console.log('a =',a);
+
+// comparison operatior
+
+// var  a=10; 
+// var   b=20;
+
+// console.log( "a =" ,a, "& b =" ,b);
+
+//  console.log('10 > 20' ,a > b)
+
+// logical operator
+
+//  
+
+
+// sir practice
+
+
+// var TotalMarks=(500)
+// console.log("TotalMarks =" + TotalMarks)
+// var num1=+prompt("Enter your Math Number")
+// var num2=+prompt("Enter your Sindhi Number")
+// var num3=+prompt("Enter your Science Number")
+// var num4=+prompt("Enter your Chemistry Number")
+// var num5=+prompt("Enter your Urdu Number")
+
+// var obtainedMarks=(num1 + num2 + num3 + num4 + num5)
+
+// console.log("ObtainedMarks =" , + obtainedMarks )
+
+// var TotalMarks =obtainedMarks/TotalMarks*100
+// console.log("Your Percentage Is " + TotalMarks + "%")
+
+
+// logical operaters
+
+// var a =5
+// var b =6
+// console.log('a < b' , a < b && a === 5)
+
+
+
+
+
+// IF ELSE ka topic
+
+// var percentage =+prompt( "Enter your percentage");
+
+// if (percentage > 100){
+//     console.log('bhai percentage to dedo sahi')
+// }
+// else if(percentage < 50){
+//     console.log("your grade is A")
+// }
+// else if(percentage >= 50 && percentage <=60){
+//     console.log("maalik B grade mubark ho")
+// }
+// else if( percentage >=61 || percentage <= 70 ){
+//     console.log('mubark')
+// }
+// else(percentage);
+
+
+// Funtion and dom intro topic
+
+
+
+// function greetUser(firstName , lastName , age){
+//     console.log('hello', firstName + lastName + age)
+    
+// };
+// greetUser(' zohaib' , ' asghar' , ' 20');
+
+
+
+// function sum(num1,num2){
+//     console.log(num1 + num2)
+// }
+// sum(20 ,30)
+
+
+
+
+// function showString() {
+//     console.log('zohaib');
+//     return'asghar'
+    
+// }
+// showString();
+// var myfunc=showString()
+//     console.log (myfunc)
+
+
+
+
+
+// function percentageCal( mark1 ,mark2 ,mark3 ,mark4){
+//     var TotalMarks=(400)
+//     console.log('total marks =' , TotalMarks)
+//     var obtainedMarks=(mark1 + mark2 + mark3 + mark4)
+//     console.log( 'obtained marks =' , obtainedMarks)
+//     return obtainedMarks/TotalMarks*100
+// }
+// var usmanpercentage=percentageCal(50 , 92 , 82 ,68)
+// console.log(usmanpercentage )
+
+
+
+
+
+
+
+// var head = document.querySelector ('.head');
+// head.style.color='red'
+// console.log(head.innerHTML)
+// head.innerHTML='change through js' ,style
+// console.log(head.innerHTML)
+
+
+
+
+
+// console.log('hello world');
+
+// function greetUser (user , user2){
+//     console.log('hello ' + user + user2);
+//     return 'hello ' + user
+// }
+
+// var username = greetUser('abdullah');
+// console.log('username ===>' , username)
+
+// var head =document.querySelector('.head') ;
+// function greatUser(){
+//     console.log(head.innerHTML)
+//     if(head.innerHTML ==='Hello world')
+//     {
+//         head.innerHTML = 'change through'
+//     }
+//     else{
+//         head.innerHTML ='hello world'
+//     }
+// }
+
+// var head= document.querySelector ('.head')
+
+
+
+// var input1 = document.querySelector('#open')
+// function click() {
+//     console.log(input1.value)
+//      input1.value=''
+
+// }
+
+
+const friuts =[ 'banana' , 'mango','lemon' ];
+friuts.push()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ array.js
@@ -0,0 +1,71 @@
+
+// friuts.push('lichee') end m ad krna
+// friuts.pop() end se htana
+// friuts.unshift('chana') start m add krna
+// friuts.shift() start se htana
+// friuts.splice(2,2 , 'puri') hatana add bi krna
+
+
+// const companies=['bloomberg','nicrosoft','uber','google','ibm','netflix']
+// const b =companies.includes('uber')
+// console.log(b)     ===>>          includes search ka kaam krta h
+
+// const companies=['bloomberg','nicrosoft','uber','google','ibm','netflix']
+// const b =companies.indexOf('uber')
+// console.log(b)   ===>>       index number check krna
+
+
+// const friuts=['banana', 'mango' ,'lemon','chikku' , 'loki']
+// console.log(friuts.slice(2 ,5))  select krna 2 se 4 tk
+
+// const a=['bloomberg','nicrosoft','uber','google','ibm','netflix']
+// const b=['yahoo','baba']
+// const c =a.concat(b)
+// console.log(c)     ===>>       dono string ko mliana
+
+// join ka kaam space khtm krna
+
+// const a=['yango','bloomberg','nicrosoft','uber','google','ibm','netflix']
+// a.sort()
+// console.log(a)  ===>>      alphabate set krta h A to Z
+
+// const a=['yango','bloomberg','nicrosoft','uber','google','ibm','netflix']
+
+// a.reverse()
+// console.log(a)  ===>>       reverse kr deta h
+
+// const a=['yango','bloomberg','nicrosoft','uber','google','ibm','netflix']
+
+
+// const a='zohaib asghar ali'
+// const b=a.split('a')
+// console.log (b)     ===>>      ghaib krdeeta h
+
+
+// const companies=['bloomberg','nicrosoft','uber','google','ibm','netflix']
+// companies.shift();
+// companies.splice(1,1,'ola')
+// companies.push('amazon')
+
+// console.log(companies)
+
+
+// sir class test 
+
+const shop=['yango','bloomberg','nicrosoft','uber','google','ibm','netflix']
+const input =document.querySelector(#input)
+const h1 =document.querySelector('h1')
+function getfriuts(){
+    console.log=(input.value)
+    if(shop.includes(input.value)){
+        console.log ('friut available h')
+        h1.innerHTML=`${input.value} is avalable`
+    }else{
+        console.log ('friut available ni h')
+        h1.innerHTML=`${input.value} is not avalable`
+    }
+
+}
+
+
+
